b2033693114e42c5c754d546ea14da4c5e8a1e90
chopin111 <chopin111@o2.pl>
2015-04-21 21:04:51 +0200

    empty graph builder file

---
+++ src/GraphBuilder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using GraphSharp;
+using GraphSharp.Algorithms.Layout.Contextual;
+
+namespace CityDriver
+{
+    class GraphBuilder
+    {
+        public void BuildGraph()
+        {
+
+        }
+    }
+}
