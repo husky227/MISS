200e3d519b86c0fc6068ebec2e13874772a82e66
husky227 <dawid.czech@gmail.com>
2015-05-27 19:09:41 +0200

    poprawki

--- src/CarDriver.cs
+++ src/CarDriver.cs
@@ -170,7 +170,7 @@ namespace CityDriver
             var distance = CountDistance(myRobot.position[0], myRobot.position[1]);
             if (distance < 0.2)
             {
-                Console.WriteLine("jest punkt " + currentNode.Id);
+//                Console.WriteLine("jest punkt " + currentNode.Id);
                 currentNode = GetNextNode();
             }
 
@@ -184,7 +184,8 @@ namespace CityDriver
             var alfa = CountAlfa(myRobot.position[0], myRobot.position[1], rotation);
 
             DeltaVelocity = (alfa * ConstAlfa);
-            Velocity = maxSpeed * (distance * ConstDistance) > maxSpeed ? maxSpeed : maxSpeed * (distance * ConstDistance);
+//            Velocity = maxSpeed * (distance * ConstDistance) > maxSpeed ? maxSpeed : maxSpeed * (distance * ConstDistance);
+            Velocity = maxSpeed;
 //            Console.WriteLine(myRobot.position[0] + " \t" + myRobot.position[1] + " \t" + GetNextNode().Position.X + " \t" + GetNextNode().Position.Y + "\t" + alfa + "\t" + rotation);
             //TODO dodac wyliczanie Velocity
         }
