895df67b49e40bc0e8115e62fee86cff79e55627
Piotr Podolski <ppodolsk@student.agh.edu.pl>
2015-06-24 23:04:19 +0200

    poprawione rysowanie

--- src/MainForm.cs
+++ src/MainForm.cs
@@ -211,7 +211,7 @@ namespace CityDriver
             // 
             this.panel1.Location = new System.Drawing.Point(247, 13);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(759, 547);
+            this.panel1.Size = new System.Drawing.Size(534, 381);
             this.panel1.TabIndex = 19;
             // 
             // MainForm
